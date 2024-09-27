f82227abf7e24927df6b4f228963f8d1d96da93f
ftanekha <farai.tanekha@gmail.com>
2024-09-27 03:38:23 +0100

    - minor change: no--edit

--- src/Components/LogoutButton.js
+++ src/Components/LogoutButton.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function LogoutButton({logout}){
-    return <button type='button' className='loginToggleButton logoutButton' onClick={logout}>Log out</button>
+    return <button type='button' className='loginToggleButton logoutButton' title='See ya! 👋🏾👨🏾' onClick={logout}>Log out</button>
 }
 
 export default LogoutButton
