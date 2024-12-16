47433857678dea9658d018255256aefe7081812f
ftanekha <farai.tanekha@gmail.com>
2024-12-16 10:42:04 +0000

    Section#Mail ISSUE: the click event for deleting emails permanently is being trigered several times on each click - use the {once: true} option to triger the event only once

--- src/js-utilityFunctions/displayMail.js
+++ src/js-utilityFunctions/displayMail.js
@@ -85,7 +85,7 @@ export default function displayMail({target}){
                         event.target.parentElement.remove()
                     }
                 }
-            }
+            }, {once: true}
         )
     )
 }
