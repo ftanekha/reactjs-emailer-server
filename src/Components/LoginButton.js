93427962315cc39b188c2df50f0092cd4f736556
ftanekha <farai.tanekha@gmail.com>
2025-01-26 18:51:57 +0000

    first commit

--- src/Components/LoginButton.js
+++
@@ -1,5 +0,0 @@
-function LogInButton(){
-    return <button type='submit' className='loginToggleButton loginButton'>Log in</button>
-}
-
-export default LogInButton
--- src/Components/LogoutButton.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-function LogoutButton({logout}){
-    return <button type='button' className='loginToggleButton logoutButton' title='See ya! 👋🏾👨🏾' onClick={logout}>Log out</button>
-}
-
-export default LogoutButton
